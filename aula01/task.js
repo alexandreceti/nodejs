ea5c68d973e9eaa8a4b927e0eca7810a74714908
alexandre <alexandrecunha.eti@gmail.com>
2019-04-09 23:41:03 +0000

    novo

---
+++ aula01/task.js
@@ -0,0 +1,67 @@
+const yargs = require("yargs");
+
+//console.log(process.argv);
+
+yargs.version('1.2.7');
+
+yargs.command({
+    command: 'add',
+    describe: 'adding new task',
+    builder: {
+        name: {
+            describe: 'task name',
+            demandOption: true,
+            type: 'string',
+        },
+        description: {
+            describe: 'task name',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: function (yargs){
+        console.log(yargs.name);
+        console.log(yargs.description);
+    }
+}).command({
+    command: 'remove',
+    describe: 'remove new task',
+    builder: {
+        name: {
+            describe: 'task name',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: function (yargs){
+        console.log(yargs.name);
+    }
+}).command({
+    command: 'list',
+    describe: 'list tasks',
+    builder: {
+        name: {
+            describe: 'task name',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: function (yargs){
+        console.log(yargs.name);
+    }
+}).command({
+    command: 'find',
+    describe: 'find task',
+    builder: {
+        name: {
+            describe: 'task name',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: function (yargs){
+        console.log(yargs.name);
+    }
+})
+
+yargs.parse();
