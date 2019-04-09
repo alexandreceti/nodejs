0f4dd1ce73cb49f53f22fec0414c0a3e1467da5c
alexandre <alexandrecunha.eti@gmail.com>
2019-04-09 22:08:37 +0000

    aula 01

--- aula01/app.js
+++ aula01/app.js
@@ -1,3 +1,7 @@
+const log = console.log
+const chalk = require("chalk")
 console.time('tempo')
-for (let i = 1; i < 100; i++) {}
+for (let i = 1; i < 100; i++) {
+    log(chalk.hsl(i, i, 50).bold('###Orange!'))
+}
 console.timeEnd('tempo')
